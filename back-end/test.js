e8e24edf0ea43890e24606e072be70c3fa09d26b
agustinclassroom <agustinclassroom@gmail.com>
2025-07-21 05:54:39 +0000

    First file opening test and new data folder.
    First test reading data from a txt inside another folder. Worked
    perfectly.

---
+++ back-end/test.js
@@ -0,0 +1,17 @@
+// testing modules!
+
+// const os = require('os')
+
+// console.log(os.homedir(), os.platform())
+
+
+// testing fs 
+const fs = require('fs');
+
+// reading files
+fs.readFile('./data/text-file.txt', (err,data) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(data.toString());
+});
