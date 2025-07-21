4dff06344e473ef571870815be2c0524fee6fb8f
agustinclassroom <agustinclassroom@gmail.com>
2025-07-21 18:54:31 +0000

    First use of express.js

---
+++ back-end/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000
+
+
+app.listen(PORT);
+
+
+app.get("/", (req,res) => {
+
+    // res.send("<p>home page</p>");
+    res.sendFile("./test.html", { root: __dirname})
+});
--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,21 @@
 //testing http
 const http = require('http');
+const fs = require("fs")
 
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method)
 
     // set header content type
     res.setHeader("Content-Type","text/html")
-    res.write ("<head>hello, peron</head>")
-    res.write ("<p>hello, agustin</p>")
-    res.write ("<p>hello, peron</p>")
-    res.end()
+    fs.readFile("./test.html",(err,data) => {
+        if (err) { 
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
 })
 
 const PORT = 3000
