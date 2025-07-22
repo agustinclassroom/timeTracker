8cbc3d1423b18cafaa3d4cb2a512c44aa7c8f4aa
agustinclassroom <agustinclassroom@gmail.com>
2025-07-22 14:20:31 +0000

    Added EJS and basic CSS formatting, first parcials

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000
+app.set("view engine", "ejs") 
 
+
+const PORT = 3000
 
 app.listen(PORT);
 
@@ -11,6 +13,16 @@ app.listen(PORT);
 app.get("/", (req,res) => {
 
     // res.send("<p>home page</p>");
-    res.sendFile("./test.html", { root: __dirname})
+    // res.sendFile("./test.html", { root: __dirname})
+    res.render("index", { title: "Home" });
+});
+app.get("/graphs", (req,res) => {
+
+    // res.send("<p>home page</p>");
+    // res.sendFile("./test.html", { root: __dirname})
+    res.render("graphs",{ title: "Graphs" })
 });
 
+app.use((req,res) => {
+    res.status(404).render("404",{ title: "404" })
+})
